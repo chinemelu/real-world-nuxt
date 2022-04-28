09c0ff17dec81fa22bdaa68a8e34d73270569298
chinemelu <chinemelunwosu@gmail.com>
2022-04-29 00:58:05 +0100

    create NavBar component; create create and index pages

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,5 +11,13 @@ module.exports = {
   extends: ['@nuxtjs', 'plugin:nuxt/recommended', 'prettier'],
   plugins: [],
   // add your custom rules here
-  rules: {},
+  rules: {
+    "vue/multi-word-component-names": ["error", {
+      "ignores": [
+        'NavBar',
+        'create',
+        'default'
+      ]
+    }]
+  }
 }
